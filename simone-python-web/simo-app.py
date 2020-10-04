afce5ed4a45e32b9eb09c963730cd917c2999ddd
simoprato <35230412+simoprato@users.noreply.github.com>
2020-10-04 11:44:36 +0200

    Basic Website
    Featuring: 
    - multiple pages
    - inheritance
    - rendering templates
    - POST and GET methods for http calls
    - Bootstrap navbar
    - login and logout
    - sessions

---
+++ simone-python-web/simo-app.py
@@ -0,0 +1,103 @@
+from flask import Flask,redirect,url_for,render_template, request, session
+#redirect, #allows redirection from certain urls
+#url_for, #allows to choose where to redirect
+#render_template #allows to render html templates in the folder template
+#request allows to use and manipulate results from http requests
+#session allows to manage sessions, during which data sent to the server are stored locally - NB: every time we close the web page, all the data of the session are erased
+
+#to run this, just enter the directory where this python file is and type python simo-app.py
+
+#launching the Flask app
+app = Flask(__name__)
+#setting up a secret key to encript all the data shared with the server through POST calls
+app.secret_key= "hello"
+
+#defining keys and values of NavBar (they will be name and href)
+navbar = {
+    'home': '/',
+    'login': '/login',
+    'logout': '/logout',
+    'user': '/user'
+}
+
+
+#HOME PAGE
+#decorator with route to home page and html string
+@app.route("/")
+def home():
+
+# version1 with in-line html
+# return "Hello, this is the main page of the Website <h1> Main Page <h1>"
+
+#version 2: rendering of html file 
+ return render_template("index_inher.html",
+  navbar=navbar)
+
+#LOGIN PAGE 
+#Testing GET(non secure, visible to all) and POST (more secure) methods for http calls
+@app.route("/login", methods= ["POST", "GET"])
+def login(): 
+#if user arrives through post call, so presses submit button, is redirected to user page with the name typed in submit form 
+  if request.method == "POST":
+    user=request.form["nm"] #note: request.form is a dictionary, so we can access via keys
+    session["user"]=user #creating a session name after the user that has filled in the login form
+    return redirect(url_for("user")) #after login, user is redirected to user page
+#if user arrives through get call, so from other page, sees login form
+  else:
+    if "user" in session:
+          return redirect(url_for("user")) 
+    
+    return render_template("login_page.html",
+  navbar=navbar)
+
+@app.route("/logout")
+def logout():
+   session.pop("user", None) #erases user session
+   return redirect(url_for("login"))
+
+@app.route("/user")
+def user():
+    if "user" in session: #checks if a specific user has already logged in
+      user=session["user"]
+      return render_template("user_page.html",
+       user=user,
+       navbar=navbar)
+    else: 
+        return redirect(url_for("login")) #if not already logged, user must log
+
+
+#TEST PAGE
+@app.route("/test")
+def test(): 
+    return render_template("new.html")
+
+
+#ANOTHER PAGE (WELCOME)
+#page getting the url last argument as input
+#version 1: simple in line html
+#@app.route("/user")
+#def user(name="user"):
+#    return render_template("user_name.html",content=name)
+#version 2: rendering html template inserting content from url
+#@app.route("/<name>")
+#def user(name):
+# return f"Welcome to this page {name}"
+ 
+    
+
+
+#ADMIN PAGE WITH REDIRECT
+#redirecting to home for some routes (needs installation of redirect and url_for packages)
+@app.route("/admin")
+def admin(): 
+     return redirect(url_for('home'))
+
+#redirecting to a specific function requiring an input 
+@app.route("/theadmin")
+def theadmin():
+    return redirect(url_for("user", name="YouAreTheAdmin!"))
+
+if __name__=="__main__":
+#adding debug=True allows debugging online and prevents us from re-running the simo-app.py every time
+ app.run(debug=True)
+
